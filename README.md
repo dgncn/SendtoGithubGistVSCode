75243dc0666370474bc8649982b45418a77bebaa
Doğancan Kasap <dgncn_ksp@hotmail.com>
2018-01-19 16:51:27 +0300

    accessible option added

--- README.md
+++ README.md
@@ -7,9 +7,13 @@ This extension sends your local file to Github Gist profile(or anonymous)
 
 * Open command palette via `CTRL + SHIFT + P` keyboard shortcut then select `Send to Github Gist`
 * If you want to send file associated with your github account, you should add `Github personal access token` with `F2`.
+* If you want to add an access token you should check `create gists` option.
 * You can remove your token with `F8` keyboard shortcut.   
-* If you select some part of a document this selected text part will become in your gist file content. But if you dont select any text in this document, extension upload all text in document to gist.   
-
+* If you select some text in document this text will become in your gist file content. But if you dont select any text in document, extension uploads all text in document to gist.   
+* You can change accessible option on creating new gists. 
+* Open command palette via `CTRL + SHIFT + P` keyboard shortcut then select `Change Github Gist Visibility(Public - Secret)`. You can upload your file as secret gist after this option.
 ## Release Notes
+### 1.0.2
+Added accessible options.
 ### 1.0.1
 Initial release of Send to Github Gist extension
--- extension.js
+++ extension.js
@@ -6,7 +6,7 @@ const path = require('path');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    
+
     let sendToGithubGist = vscode.commands.registerCommand('extension.getSelection', function () {
         let editor = vscode.window.activeTextEditor;
         if (!editor) {
@@ -23,12 +23,18 @@ function activate(context) {
             debug: true
         });
 
+
         var accessToken = context.globalState.get("token");
+        var visibility = true;
         if (accessToken != undefined && accessToken != "") {
             github.authenticate({
                 type: 'oauth',
                 token: accessToken
             });
+            visibility = context.globalState.get("visibility");
+            if (visibility == undefined) {
+                visibility = true;
+            }
         }
         else {
             vscode.window.showWarningMessage("If you want add gist to your github account you should get a github personel access token from Github settings page.");
@@ -44,10 +50,11 @@ function activate(context) {
             configurable: true,
             enumerable: true
         });
+
         var repoPromise = github.gists.create({
             'files': fileWithContent,
             'description': exactFileName,
-            'public': true
+            'public': visibility
         });
         repoPromise.then((data) => {
             var gist = data;
@@ -73,7 +80,7 @@ function activate(context) {
                 vscode.window.showErrorMessage(error);
             });
         }
-        else{
+        else {
             vscode.window.showInformationMessage("Error: Access token could not be added");
         }
 
@@ -83,16 +90,39 @@ function activate(context) {
         var tokenInput = context.globalState.get("token");
         if (tokenInput != undefined && tokenInput != "") {
             tokenInput = "";
-                context.globalState.update("token",tokenInput);
-                vscode.window.showInformationMessage("The access token has removed.");
+            context.globalState.update("token", tokenInput);
+            vscode.window.showInformationMessage("The access token has removed.");
         }
-        else{
+        else {
             vscode.window.showInformationMessage("The access token was not found.");
         }
     });
+
+    let changeGistVisibility = vscode.commands.registerCommand('extension.changeGistVisibility', function () {
+        var visibilityValue = context.globalState.get("visibility");
+        if (visibilityValue == undefined) {
+            visibilityValue = true;
+        }
+        var accessToken = context.globalState.get("token");
+        if (accessToken != undefined && accessToken != "") {
+            visibilityValue = !visibilityValue;
+        }
+        else {
+            visibilityValue = true;
+        }
+        context.globalState.update("visibility", visibilityValue);
+        if (visibilityValue == true) {
+            vscode.window.showInformationMessage("Gist accessible has changed to Public.");
+        }
+        else {
+            vscode.window.showInformationMessage("Gist accessible has changed to Secret.");
+        }
+    });
+
     context.subscriptions.push(sendToGithubGist);
     context.subscriptions.push(addPersonelGithubToken);
     context.subscriptions.push(removePersonelGithubToken);
+    context.subscriptions.push(changeGistVisibility);
 }
 exports.activate = activate;
 
